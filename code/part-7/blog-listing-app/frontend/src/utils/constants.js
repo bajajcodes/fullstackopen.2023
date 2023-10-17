33d8fca03a6625e21ad4e0229ac4c69e2ee1ab50
Shubham Bajaj <shmbajaj32@gmail.com>
2023-10-17 09:10:26 +0000

    Removed cloned repo

--- code/part-7/blog-listing-app/frontend/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "http://localhost:3001/api";
-const DEFAULT_ERROR_MESSAGE = "Something Went Wrong!!";
-const BROWSER_STORAGE_USER_NAME_kEY = "loggedBlogListappUser";
-
-export default {
-  API_BASE_URL,
-  DEFAULT_ERROR_MESSAGE,
-  BROWSER_STORAGE_USER_NAME_kEY,
-};
