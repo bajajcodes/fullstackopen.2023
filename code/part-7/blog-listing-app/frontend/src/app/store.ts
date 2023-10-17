92cea597314d3f7d3580303846d5c8e03cee7b7c
Shubham Bajaj <shmbajaj32@gmail.com>
2023-10-17 08:53:22 +0000

    created redux store

---
+++ code/part-7/blog-listing-app/frontend/src/app/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import notificationReducer from "../reducers/notification.reducer";
+
+const store = configureStore({
+  reducer: { notification: notificationReducer },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
