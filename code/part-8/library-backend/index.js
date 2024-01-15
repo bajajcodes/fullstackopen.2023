fed3909f30bcadd4c2d364f3c8f8ab68156a5770
shubham bajaj <shmbajaj32@gmail.com>
2024-01-15 17:48:37 +0530

    finished graphql exercises 8.1-8.7

--- code/part-8/library-backend/index.js
+++ code/part-8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
 
 const authors = [
   {
@@ -31,58 +32,149 @@ const books = [
   {
     title: 'Clean Code',
     published: 2008,
-    authorId: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
+    author: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
     id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring'],
   },
   {
     title: 'Agile software development',
     published: 2002,
-    authorId: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
+    author: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
     id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
     genres: ['agile', 'patterns', 'design'],
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
-    authorId: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
+    author: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
     id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring'],
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
-    authorId: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
+    author: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
     id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring', 'patterns'],
   },
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
-    authorId: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
+    author: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
     id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring', 'design'],
   },
   {
     title: 'Crime and punishment',
     published: 1866,
-    authorId: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+    author: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
     id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
     genres: ['classic', 'crime'],
   },
   {
     title: 'The Demon ',
     published: 1872,
-    authorId: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+    author: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
     id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
     genres: ['classic', 'revolution'],
   },
 ];
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int
+    genres: [String!]
+    author: ID!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int
+    id: ID!
+  }
+
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]
+    allAuthors: [Author!]
+  }
+
+  type Mutation{
+    addBook(title: String!, author: String!, published: Int!, genres: [String!]):Book
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
 `;
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (_, args) => {
+      const authorName = args?.author;
+      let authorId = authors.find((author) => author.name === authorName)?.id;
+      const genre = args?.genre;
+      const genreCb = (book) => book.genres.includes(genre);
+      const authorCb = (book) => book.author === authorId;
+
+      let filteredBooks = books.slice();
+
+      if (authorId) {
+        filteredBooks = filteredBooks.filter(authorCb);
+      }
+
+      if (genre) {
+        filteredBooks = filteredBooks.filter(genreCb);
+      }
+
+      return filteredBooks;
+    },
+    allAuthors: () => authors,
+  },
+  Book: {
+    title: (root) => root.title,
+    published: (root) => root.published,
+    genres: (root) => root.genres,
+    author: (root) => authors.find((a) => a.id === root.author).name,
+    id: (root) => root.id,
+  },
+  Author: {
+    bookCount: (root) =>
+      books.reduce((acc, book) => (book.author === root.id ? ++acc : acc), 0),
+  },
+  Mutation: {
+    addBook: (_, args) => {
+      let author = authors.find((a) => a.name === args.author);
+      if (!author) {
+        author = {
+          name: args.author,
+          id: uuid(),
+        };
+        authors.push(author);
+      }
+      const book = {
+        ...args,
+        author: author.id,
+        id: uuid(),
+      };
+      books.push(book);
+      return book;
+    },
+    editAuthor: (_, args) => {
+      const index = authors.findIndex((e) => e.name === args.name);
+      console.log({ args, index });
+      if (index < 0) return null;
+      const author = authors[index];
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors.splice(index, 1, updatedAuthor);
+      return updatedAuthor;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
