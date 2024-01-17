6662d399da3a46e1dede443d07295a5533d652c7
shubham bajaj <shmbajaj32@gmail.com>
2024-01-17 12:16:35 +0530

    using jwt for authentication

--- code/part-8/phonebook/backend/src/utils/config.js
+++ code/part-8/phonebook/backend/src/utils/config.js
@@ -3,5 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export { MONGODB_URI };
+export { MONGODB_URI, JWT_SECRET };
