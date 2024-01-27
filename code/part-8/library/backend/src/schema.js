9840ba4b7df6e9c9668d570617d66959f3bc4c3c
shubham bajaj <shmbajaj32@gmail.com>
2024-01-27 14:20:50 +0530

    refactor: schema and resolver into its own file

---
+++ code/part-8/library/backend/src/schema.js
@@ -0,0 +1,52 @@
+const typeDefs = `
+
+type User {
+  username: String!
+  favoriteGenre: String!
+  id: ID!
+}
+
+type Token {
+  value: String!
+}
+
+type Author {
+  name: String!
+  born: Int
+  id: ID!
+ }
+
+  type Book {
+    title: String!
+    published: Int!
+    genres: [String!]
+    author: Author
+    id: ID!
+  }
+
+  type Qualities {
+    fit: Boolean
+    rich: Boolean
+    smart: Boolean
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]
+    allAuthors: [Author!]
+    allUsers: [User!]
+    allGenres: [String!]!
+    me: User
+  }
+
+  type Mutation{
+    addBook(title: String!, author: String!, published: Int!, genres: [String!]):Book
+    createUser(username: String!, favoriteGenre: String!): User
+    login(username: String!, password: String!): Token
+    addAuthor(name: String!, born: Int): Author
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+export default typeDefs;
